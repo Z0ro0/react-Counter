267530517b8a996a94776a2d18b28f7902f7f4c2
Z0ro0 <chlrkdmf0805@gmail.com>
2025-02-21 15:00:56 +0900

    react lifecycle useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Box from './component/Box'
 
@@ -17,8 +17,20 @@ function App() {
   //4. 변수값은 1로 보이고 state 값은 안 변했기 때문에 그 전 값이 보임
   //5. 리랜더링(counter는 0으로 초기화됨)
   
+  //useEffect 매개변수 두 가지: 콜백함수 ()=>{}, array[]
+  //useEffect = componentDidMount     랜더링 시 실행됨
+  useEffect(()=>{
+    console.log("useEffect1");
+  },[]);
+
+  //useEffect = componentDidUpdate      counter2가 update 될 때마다 호출됨
+  useEffect(()=>{
+    console.log("useEffect2");
+  },[counter2]);
+ 
   return (
     <div>
+      {console.log("render")}
       <div>{counter}</div>
       <div>{counter2}</div>
       <button onClick={increase}>클릭</button>
