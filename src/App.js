382d7351b81ddd440da4589fe4b2b2852188cd2a
Z0ro0 <chlrkdmf0805@gmail.com>
2025-02-21 11:03:42 +0900

    useState - state는 비동기적으로 실행하기 때문에 끝날 때까지 기다렸다가 마지막에 실행 -> console.log로 찍어도 이전의 값을 들고 있음 - 컴포넌트 리랜더링 시 변수값 초기화 되기 때문에 counter는 계속 0으로 초기화 되어 1이 됨됨

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Box from './component/Box'
 
 function App() {
+  let counter = 0;
+  const [counter2, setCounter2] = useState(0);
+
+  const increase = () => {
+    counter = counter + 1;
+    setCounter2(counter2+1);
+  }
+
+  //1. 버튼 클릭
+  //2. counter+1fotj 1dl ehla
+  //3. setState 함수 호출
+  //4. 변수값은 1로 보이고 state 값은 안 변했기 때문에 그 전 값이 보임
+  //5. 리랜더링(counter는 0으로 초기화됨)
+  
   return (
     <div>
-      <Box name="리사" num="1" />
-      <Box name="제니" num="2" />
-      <Box name="지수" num="3" />
+      <div>{counter}</div>
+      <div>{counter2}</div>
+      <button onClick={increase}>클릭</button>
     </div>
   );
 }
