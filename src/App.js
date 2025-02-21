008cdfc11489064ef865150e5beeca699237fbb5
Z0ro0 <chlrkdmf0805@gmail.com>
2025-02-21 10:13:55 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Box from './component/Box'
+
+function App() {
+  return (
+    <div>
+      <Box />
+      <Box />
+      <Box />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Box.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className='box'>
+        Box1
+        <p>짠</p>
+    </div>
+  )
+}
+
+export default Box
